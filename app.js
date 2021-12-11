3bbecf30043f3d4a5dfc2853f13149bd07c44701
Pavaman Dabeer <pavamandabeer@gmail.com>
2021-12-11 22:56:29 +0530

    execution entry file

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
+require("express-async-errors");
 
 const express = require("express");
+const connectDB = require("./db/connect");
 const errorHandlerMiddleware = require("./middlewares/error-handler");
 const notFoundMiddleware = require("./middlewares/not-found");
+const productsRouter = require("./routes/products");
 const app = express();
 
 app.use(express.json());
@@ -13,13 +16,15 @@ app.get("/", (req, res) => {
   );
 });
 
+app.use("/api/v1/products", productsRouter);
+
 app.use(errorHandlerMiddleware);
 app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 3000;
 const initialise = async () => {
   try {
-    //connect DB;
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is up and listening at Port ${port}`));
   } catch (error) {}
 };
